34427fe538719beb4f7967ee23427a409590bdcc
Marcus <mackansw@gmail.com>
2020-04-12 22:49:19 +0200

    Removed Easter egg!

--- src/mackansw/defuse/Game.java
+++ src/mackansw/defuse/Game.java
@@ -7,7 +7,7 @@ import java.awt.event.*;
 /**
  * A simple click/stress game created by Mackansw as a school examination project.
  * @author Mackansw
- * @version 20.04.05
+ * @version 20.04.12
  * */
 
 public class Game {
